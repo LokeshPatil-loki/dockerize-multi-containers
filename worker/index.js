968620f6b681b6cd26b49640ebadce35c8fec714
LokeshPatil-loki <lokesh1404@outlook.com>
2023-08-09 21:38:00 +0530

    convert module type

--- worker/index.js
+++ worker/index.js
@@ -1,23 +1,28 @@
-const keys = require("./keys");
 
-const redis = require("redis");
+import keys from "./keys.js";
+import redis from "redis";
 
 const redisClient = redis.createClient({
     url: `redis://${keys.redisHost}:${keys.redisPort}`,
     retry_strategy: () => 1000
 });
-redisClient.connect();
+await redisClient.connect();
 
 const subscriber = redisClient.duplicate();
-
+await subscriber.connect();
 function fib(index){
-    if(index < 2)
-        return 1;
+    if(index <= 1)
+        return index;
     return fib(index-1) + fib(index-2);
 }
 
-subscriber.on("message",(channel, message) => {
-    redisClient.hSet("values",message,fib(parseInt(message)));
-})
+// subscriber.on("message",(channel, message) => {
+//     console.log("message")
+//     // redisClient.hSet("values",message,fib(parseInt(message)));
+// })
 
-subscriber.subscribe("insert");
+subscriber.subscribe("insert",(message => {
+    const num = parseInt(message);
+    console.log("fib of ",num)
+    redisClient.hSet("values",message,fib(num));
+}));
